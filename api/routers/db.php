4249f65f8666113fa346897792ac89f804e32c2e
Алексей Лаптев <radio.volchanka@gmail.com>
2019-07-09 20:15:34 +0500

    api

---
+++ api/routers/db.php
@@ -0,0 +1,6 @@
+<?php
+  $dbname = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA=(SID=XE)))"; 
+  $dbuser = "shop";
+  $dbpassword = "Shop1";
+  $dbencoding = "AL32UTF8";
+?>
